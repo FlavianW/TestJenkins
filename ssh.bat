66814e15241a95c9ffa3e6ce8b9845b89eb82012
FlavianW <98649301+FlavianW@users.noreply.github.com>
2023-12-11 03:40:28 +0100

    Update ssh.bat

--- ssh.bat
+++ ssh.bat
@@ -1,2 +1,2 @@
 @echo off
-plink -ssh root@127.0.0.1 -P 42 -pw "AZER" "rm -rf /var/www/* && mkdir /var/www/html && git clone https://github.com/FlavianW/TestJenkins.git /var/www/html/"
+plink -ssh root@127.0.0.1 -P 42 -pw "AZER" -i "C:\Users\flavi\.ssh\prive.pkk" "rm -rf /var/www/* && mkdir /var/www/html && git clone https://github.com/FlavianW/TestJenkins.git /var/www/html/"
